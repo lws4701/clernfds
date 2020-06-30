c66cd83d91b9cee2086aa12ccb18bc354f078b69
hinsonc <hinsonc0513@gmail.com>
2020-06-29 21:47:31 -0500

    Added detection model, modified detection program to output coordinates of detected person

--- Server/tensorflow-human-detection.py
+++ Server/tensorflow-human-detection.py
@@ -14,14 +14,14 @@ class DetectorAPI:
 
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
+            od_graph_def = tf.compat.v1.GraphDef()
+            with tf.compat.v1.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
         self.default_graph = self.detection_graph.as_default()
-        self.sess = tf.Session(graph=self.detection_graph)
+        self.sess = tf.compat.v1.Session(graph=self.detection_graph)
 
         # Definite input and output Tensors for detection_graph
         self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
@@ -37,23 +37,22 @@ class DetectorAPI:
         # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image, axis=0)
         # Actual detection.
-        start_time = time.time()
-        (boxes, scores, classes, num) = self.sess.run(
+        (frame_boxes, frame_scores, frame_classes, frame_num) = self.sess.run(
             [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
             feed_dict={self.image_tensor: image_np_expanded})
         end_time = time.time()
 
-        print("Elapsed Time:", end_time-start_time)
+        #print("Elapsed Time:", end_time-start_time)
 
-        im_height, im_width,_ = image.shape
-        boxes_list = [None for i in range(boxes.shape[1])]
-        for i in range(boxes.shape[1]):
-            boxes_list[i] = (int(boxes[0, i, 0] * im_height),
-                        int(boxes[0, i, 1]*im_width),
-                        int(boxes[0, i, 2] * im_height),
-                        int(boxes[0, i, 3]*im_width))
+        im_height, im_width, _ = image.shape
+        boxes_list = [None for i in range(frame_boxes.shape[1])]
+        for i in range(frame_boxes.shape[1]):
+            boxes_list[i] = (int(frame_boxes[0, i, 0] * im_height),
+                        int(frame_boxes[0, i, 1]*im_width),
+                        int(frame_boxes[0, i, 2] * im_height),
+                        int(frame_boxes[0, i, 3]*im_width))
 
-        return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
+        return boxes_list, frame_scores[0].tolist(), [int(x) for x in frame_classes[0].tolist()], int(frame_num[0])
 
     def close(self):
         self.sess.close()
@@ -61,26 +60,27 @@ class DetectorAPI:
 
 
 if __name__ == "__main__":
-    model_path = '/path/to/faster_rcnn_inception_v2_coco_2017_11_08/frozen_inference_graph.pb'
+    model_path = 'test/faster_rcnn_inception_v2_coco/frozen_inference_graph.pb'
     odapi = DetectorAPI(path_to_ckpt=model_path)
     threshold = 0.7
-    cap = cv2.VideoCapture('/path/to/input/video')
+    cap = cv2.VideoCapture('img/testpic5.jpg')
 
-    while True:
-        r, img = cap.read()
-        img = cv2.resize(img, (1280, 720))
+    #while True:
+    r, img = cap.read()
+    img = cv2.resize(img, (1280, 720))
 
-        boxes, scores, classes, num = odapi.processFrame(img)
+    boxes, scores, classes, num = odapi.processFrame(img)
 
-        # Visualization of the results of a detection.
+    # Visualization of the results of a detection.
 
-        for i in range(len(boxes)):
-            # Class 1 represents human
-            if classes[i] == 1 and scores[i] > threshold:
-                box = boxes[i]
-                cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
-
-        cv2.imshow("preview", img)
-        key = cv2.waitKey(1)
-        if key & 0xFF == ord('q'):
-            break
+    for i in range(len(boxes)):
+        # Class 1 represents human
+        if classes[i] == 1 and scores[i] > threshold:
+            box = boxes[i]
+            cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+            print("(", box[1], ", ", box[0], ")")
+            print("(", box[3], ", ", box[2], ")")
+    cv2.imshow("preview", img)
+    key = cv2.waitKey(5000)
+    #if key & 0xFF == ord('q'):
+        #break
