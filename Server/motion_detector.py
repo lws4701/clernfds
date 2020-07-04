ed7c6ae43ccfcd28f3d8a1fc9636c741f6a5d143
Noah973 <NE56973@live.missouristate.edu>
2020-07-04 11:25:29 -0500

    US-05 Reworked some of the translation calculations to use center point instead of top_left corner. Added some comments to explain some things. Linked up the tensorflow model into main function to get data from actual frames. Finished this story.

--- Server/motion_detector.py
+++ Server/motion_detector.py
@@ -1,7 +1,25 @@
 import math
+import os
+from tensorflow_human_detection import DetectorAPI
 
-SIZE_OF_PACKET = 5
-ORIGIN = (0, 0)
+
+class MotionDetector:
+
+    frames = list()
+
+    def __init__(self, dict_of_frames):
+        for key in dict_of_frames:
+            self.frames.append(Frame(key, DetectedPerson(*dict_of_frames[key][0])))
+
+    def motionDataFromFrames(self) -> list:
+        motion_data_list = []
+        for i in range(1, len(self.frames)):
+            motion_data_list.append(MotionData(self.frames[i - 1], self.frames[i]))
+        return motion_data_list
+
+    def getFrameObjects(self):
+        return self.frames
+
 
 class Frame:
     frame_id = None
@@ -13,6 +31,13 @@ class Frame:
 
     def person_detected(self):
         return self.detected_person is None
+
+    def __str__(self):
+        return '{' + '\n' \
+                + '\t' + 'frame_id: ' + str(self.frame_id) + '\n' \
+                + '\t' + 'detected_person: ' + str(self.detected_person) + '\n' \
+                + '}'
+
 
 
 class DetectedPerson:
@@ -39,89 +64,124 @@ class DetectedPerson:
         self.height = abs(top_left[1] - bottom_left[1])
         self.center = (top_left[0] + self.width / 2, top_left[1] + self.height / 2)
         self.area = self.width * self.height
-        self.length_of_diag = math.sqrt(self.width ** 2 + self.height ** 2)
-        self.angle_of_diag = round(math.atan2(top_right[0] - bottom_left[0], top_right[1] - bottom_left[1]), 2)
+        self.length_of_diag = round(math.sqrt(self.width ** 2 + self.height ** 2), 4)
+        self.angle_of_diag = round(math.atan2(self.height, self.width), 4)
+
+    def __str__(self):
+        return '{' + '\n' \
+                + '\t' + 'top_left: ' + str(self.top_left) + ',' + '\n' \
+                + '\t' + 'top_right: ' + str(self.top_right) + '\n' \
+                + '\t' + 'bottom_left: ' + str(self.bottom_left) + ',' + '\n' \
+                + '\t' + 'bottom_right: ' + str(self.bottom_right) + ',' + '\n' \
+                + '\t' + 'width: ' + str(self.width) + ' pixels' + '\n' \
+                + '\t' + 'height: ' + str(self.height) + ' pixels' + '\n' \
+                + '\t' + 'center: ' + str(self.center) + '\n' \
+                + '\t' + 'area: ' + str(self.area) + ' pixels^2' + '\n' \
+                + '\t' + 'length_of_diag: ' + str(self.length_of_diag) + ' pixels' + '\n' \
+                + '\t' + 'angle_of_diag: ' + str(self.angle_of_diag) + ' radians' + '\n' \
+                + '}'
+
 
 
 class MotionData:
     start_frame = None
     end_frame = None
-    delta_x = None
-    delta_y = None
-    motion_vector = (None, None)
 
-    # unit: pixels per frame
-    velocity = None
+    delta_x = None                  # ------------------------------------- #
+    delta_y = None                  #                                       #
+    motion_vector = (None, None)    #                                       #
+                                    #                                       #
+    # unit: pixels per frame        #   All these motion data points        #
+    velocity = None                 #   describe the motion between         #
+                                    #   frames of the center point          #
+    # counterclockwise angle in     #   of the bounding box to sort of      #
+    # radians between the positive  #   represent an overall avg of how     #
+    # X-axis of the starting        #   the box changed/moved.              #
+    # center point and the          #                                       #
+    # end center point              #                                       #
+    direction = None                # ------------------------------------- #
 
-    # counterclockwise angle in radians (not degrees) between the positive X-axis
-    # (or the X-axis of the start point) and the point (x, y).
-    direction = None
+    transform_detected = None       # Any change in "shape" of bounding box (this will happen most of the time)
+    translate_detected = None       # A strict translation with no transformation occurring
 
-    transform_detected = None
-    translate_detected = None
+    diag_angle_change = None
+    diag_length_change = None
+
+    height_change = None
+    width_change = None
 
     def __init__(self, start_frame, end_frame):
         start_top_left = start_frame.detected_person.top_left
         end_top_left = end_frame.detected_person.top_left
+        start_center = start_frame.detected_person.center
+        end_center = end_frame.detected_person.center
         self.start_frame = start_frame
         self.end_frame = end_frame
-        self.delta_x = abs(start_top_left[0] - end_top_left[0])
-        self.delta_y = abs(start_top_left[1] - end_top_left[1])
-        self.motion_vector = (end_top_left[0] - start_top_left[0], end_top_left[1] - start_top_left[1])
-        self.velocity = self.calc_velocity(start_top_left, end_top_left)
-        self.direction = self.calc_direction(start_top_left, end_top_left)
         self.transform_detected = (not (start_frame.detected_person.length_of_diag == end_frame.detected_person.length_of_diag) or
                                    not (start_frame.detected_person.angle_of_diag == end_frame.detected_person.angle_of_diag))
-        self.translate_detected = (not (start_top_left == end_top_left) and
-                                   not (start_frame.detected_person.top_right == end_frame.detected_person.top_right) and
-                                   not (start_frame.detected_person.bottom_left == end_frame.detected_person.bottom_left) and
-                                   not (start_frame.detected_person.bottom_right == end_frame.detected_person.bottom_right))
 
+        motion_vector = (end_center[0] - start_center[0], end_center[1] - start_center[1])
+        self.translate_detected = (not self.transform_detected and not motion_vector == (0, 0) and self.calcMotionVector(start_top_left, end_top_left) == motion_vector and
+                                   self.calcMotionVector(start_frame.detected_person.top_right, end_frame.detected_person.top_right) == motion_vector and
+                                   self.calcMotionVector(start_frame.detected_person.bottom_left, end_frame.detected_person.bottom_left) == motion_vector and
+                                   self.calcMotionVector(start_frame.detected_person.bottom_right, end_frame.detected_person.bottom_right) == motion_vector)
 
-    def calc_velocity(self, start_point, end_point) -> float:
+        self.delta_x = abs(end_center[0] - start_center[0])
+        self.delta_y = abs(end_center[1] - start_center[1])
+        self.motion_vector = motion_vector
+        self.velocity = self.calcVelocity(start_center, end_center)
+        self.direction = self.calcDirection(start_center, end_center)
+
+        if self.transform_detected:
+            self.diag_angle_change = round(end_frame.detected_person.angle_of_diag - start_frame.detected_person.angle_of_diag, 4)
+            self.diag_length_change = round(end_frame.detected_person.length_of_diag - start_frame.detected_person.length_of_diag, 4)
+            self.height_change = end_frame.detected_person.height - start_frame.detected_person.height
+            self.width_change = end_frame.detected_person.width - start_frame.detected_person.width
+
+    def calcVelocity(self, start_point, end_point) -> float:
         start_x = start_point[0]
         start_y = start_point[1]
         end_x = end_point[0]
         end_y = end_point[1]
         sign = -1 if end_x < start_x else 1
-        return sign * round(math.sqrt(abs(end_x - start_x) ** 2 + abs(end_y - start_y) ** 2) / abs(self.end_frame.frame_id - self.start_frame.frame_id), 2)
+        return sign * round(math.sqrt(abs(end_x - start_x) ** 2 + abs(end_y - start_y) ** 2) / 1, 4)
 
-    def calc_direction(self, start_point, end_point) -> float:
-        return round(math.atan2(end_point[0] - start_point[0], end_point[1] - start_point[1]), 2)
+    def calcDirection(self, start_point, end_point) -> float:
+        return round(math.atan2(end_point[1] - start_point[1], end_point[0] - start_point[0]), 4)
 
+    def calcMotionVector(self, start_point, end_point) -> tuple:
+        return end_point[0] - start_point[0], end_point[1] - start_point[1]
 
     def __str__(self):
         return '{' + '\n' \
                 + '\t' + 'frames: ' + str(self.start_frame.frame_id) + '-' + str(self.end_frame.frame_id) + ',' + '\n' \
+                + '\t' + 'translate_detected: ' + str(self.translate_detected) + '\n' \
                 + '\t' + 'delta_x: ' + str(self.delta_x) + ',' + '\n' \
                 + '\t' + 'delta_y: ' + str(self.delta_y) + ',' + '\n' \
                 + '\t' + 'motion_vector: ' + str(self.motion_vector) + '\n' \
                 + '\t' + 'velocity: ' + str(self.velocity) + ' pixels/frame' + '\n' \
                 + '\t' + 'direction: ' + str(self.direction) + ' radians' + '\n' \
                 + '\t' + 'transform_detected: ' + str(self.transform_detected) + '\n' \
-               + '\t' + 'translate_detected: ' + str(self.translate_detected) + '\n' \
-               + '}'
+                + '\t' + 'diag_angle_change: ' + str(self.diag_angle_change) + ' radians' + '\n' \
+                + '\t' + 'diag_length_change: ' + str(self.diag_length_change) + ' pixels' + '\n' \
+                + '\t' + 'height_change: ' + str(self.height_change) + ' pixels' + '\n' \
+                + '\t' + 'width_change: ' + str(self.width_change) + ' pixels' + '\n' \
+                + '}'
+
 
 def main():
     if __name__ == "__main__":
-        test_data = [
-            Frame(0, DetectedPerson((1, 2), (5, 2), (1, 8), (5, 8))),
-            Frame(1, DetectedPerson((5, 2), (9, 2), (5, 8), (9, 8))),
-            Frame(2, DetectedPerson((5, 2), (9, 2), (5, 8), (9, 8))),
-            Frame(3, DetectedPerson((5, 4), (9, 4), (5, 8), (9, 8))),
-            Frame(4, DetectedPerson((0, 6), (3, 6), (0, 12), (3, 12))),
-        ]
-        result = motion_data_from_frames(test_data)
+        model_path = 'test/faster_rcnn_inception_v2_coco/frozen_inference_graph.pb'
+        odapi = DetectorAPI(path_to_ckpt=model_path)
+        frame_packet = os.listdir('img')
+        test_data = odapi.processPacket(frame_packet)
+        motion_detector = MotionDetector(test_data)
+        result = motion_detector.motionDataFromFrames()
+        frames = motion_detector.getFrameObjects()
         for obj in result:
             print(str(obj))
-
-
-def motion_data_from_frames(list_of_frames) -> list:
-    motion_data_list = []
-    num_frames = len(list_of_frames)
-    for i in range(1, num_frames):
-        motion_data_list.append(MotionData(list_of_frames[i-1], list_of_frames[i]))
-    return motion_data_list
+        for obj in frames:
+            print(str(obj))
 
 
 
