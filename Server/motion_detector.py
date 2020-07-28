8128af7986cc2cae1f3db7e13bef89dd1c8e446d
Noah973 <NE56973@live.missouristate.edu>
2020-07-27 19:42:30 -0500

    Noah737 - Stripped down motion detector real good

--- Server/motion_detector.py
+++ Server/motion_detector.py
@@ -17,7 +17,6 @@ import math
 
 def motion_data_from_frames(dict_of_frames) -> list:
     """
-    Returns: A list of MotionData objects created from the list of frames objects.
     Loops through the list of frames and uses each adjacent pair and inputs them
     into the MotionData objects constructor.
     :returns motion_data_list: list of MotionData objects
@@ -63,7 +62,7 @@ class DetectedPerson:
     """
     This class represents a foreground object that has been detected within a frame. It holds
     member variables for the four corners of the bounding box around this detected object. Along
-    data points about the rectangle including area, height, width etc. (referenced in design class
+    data points about the rectangle including center, height, width etc. (referenced in design class
     hierarchy diagram under section 3.4.3.1)
     """
 
@@ -74,8 +73,6 @@ class DetectedPerson:
     width = None
     height = None
     center = (None, None)
-    area = None
-    length_of_diag = None
     angle_of_diag = None
 
     def __init__(self, top_left, top_right, bottom_left, bottom_right):
@@ -86,10 +83,6 @@ class DetectedPerson:
         self.width = abs(top_right[0] - top_left[0])
         self.height = abs(top_left[1] - bottom_left[1])
         self.center = (top_left[0] + self.width / 2, top_left[1] + self.height / 2)
-        self.area = self.width * self.height
-
-        # Pythagorean formula using width and height as the two lengths of sides of triangle
-        self.length_of_diag = round(math.sqrt(self.width ** 2 + self.height ** 2), 4)
 
         # Arc-tan of opposite (height) over adjacent (width). This is the angle formed between the
         # diagonal running from bottom left corner to top right and the bottom edge of the rectangle
@@ -104,8 +97,6 @@ class DetectedPerson:
                 + '\t' + 'width: ' + str(self.width) + ' pixels' + '\n' \
                 + '\t' + 'height: ' + str(self.height) + ' pixels' + '\n' \
                 + '\t' + 'center: ' + str(self.center) + '\n' \
-                + '\t' + 'area: ' + str(self.area) + ' pixels^2' + '\n' \
-                + '\t' + 'length_of_diag: ' + str(self.length_of_diag) + ' pixels' + '\n' \
                 + '\t' + 'angle_of_diag: ' + str(self.angle_of_diag) + ' radians' + '\n' \
                 + '}'
 
@@ -114,49 +105,30 @@ class MotionData:
     """
     This class represents the analysis of the motion between two frame objects.
     Holds data about the motion of the center point of the bounding box along with data
-    on how the box has transformed between frames such as diag_angle_change, height_change etc.
+    on how the box has transformed between frames such as diag_angle_change, velocity etc.
     (referenced in design class hierarchy diagram under section 3.4.3.1)
     """
+
     # frame to start from
     start_frame = None
+
     # frame to end at
     end_frame = None
 
-    # absolute change in x
-    delta_x = None                  # ------------------------------------- #
-    # absolute change in y          #                                       #
-    delta_y = None                  #                                       #
-    motion_vector = (None, None)    #                                       #
-                                    #                                       #
-    # unit: pixels per frame        #   All these motion data points        #
-    velocity = None                 #   describe the motion between         #
-                                    #   frames of the center point          #
-    # counterclockwise angle in     #   of the bounding box to sort of      #
-    # radians between the positive  #   represent an overall avg of how     #
-    # X-axis of the starting        #   the box changed/moved.              #
-    # center point and the          #                                       #
-    # end center point              #                                       #
-    direction = None                # ------------------------------------- #
+    # Change in x and y of center points between frames
+    motion_vector = (None, None)
 
-    # Any change in "shape" of bounding box (this will happen most of the time)
-    transform_detected = None
-    # A strict translation with no transformation occurring
-    translate_detected = None
+    # unit: pixels per frame
+    velocity = None
 
     # change in the angle of diagonal from start frame to end frame
     diag_angle_change = None
-    # change in the length of diagonal from start frame to end frame
-    diag_length_change = None
 
-    # change in height ('+' = increased height, '-' = decreased height)
-    height_change = None
-    # change in width
-    width_change = None
 
     def __init__(self, start_frame, end_frame):
         """
         This constructor function only takes in two Frame objects and calculates
-        and sets all the data point parameters with help of some helper functions.
+        and sets all the data point parameters.
         :param start_frame: the frame object to start from
         :param end_frame: the frame object to end at
         """
@@ -167,68 +139,20 @@ class MotionData:
         self.start_frame = start_frame
         self.end_frame = end_frame
 
-        # Checks for if the height or width of the rectangle has changed since this describes a transformation
-        self.transform_detected = (not (start_frame.detected_person.height == end_frame.detected_person.height) or
-                                   not (start_frame.detected_person.width == end_frame.detected_person.width))
-
-        # Checks to see if a transformation has not been detected and if the center point has changed
-        self.translate_detected = (not self.transform_detected and not start_center == end_center)
-
-        # Absolute value of the difference in the center x/y
-        self.delta_x = abs(end_center[0] - start_center[0])
-        self.delta_y = abs(end_center[1] - start_center[1])
-
         # A tuple with signed integers indicating the change in x and y
         self.motion_vector = (end_center[0] - start_center[0], end_center[1] - start_center[1])
 
         # Basically just the length of the line formed by the start center point and end center point
-        self.velocity = self.calc_velocity(start_center, end_center)
+        self.velocity = round(math.sqrt(abs(end_center[0] - start_center[0]) ** 2 + abs(end_center[1] - start_center[1]) ** 2), 4)
 
-        # The Angle, measured in radians, of the line of velocity
-        self.direction = self.calc_direction(start_center, end_center)
-
+        # Change in the angle of the diag from start_frame to end_frame
         self.diag_angle_change = round(end_frame.detected_person.angle_of_diag - start_frame.detected_person.angle_of_diag, 4)
-        self.diag_length_change = round(end_frame.detected_person.length_of_diag - start_frame.detected_person.length_of_diag, 4)
-        self.height_change = end_frame.detected_person.height - start_frame.detected_person.height
-        self.width_change = end_frame.detected_person.width - start_frame.detected_person.width
 
-    def calc_velocity(self, start_point, end_point) -> float:
-        """
-        This functions takes in a start_point and end_point and uses the pythagorean
-        theorem to find the straight line distance between them on a 2D plane.
-        :param start_point: Tuple of form: (x, y)
-        :param end_point: Tuple of form: (x, y)
-        :return: positive velocity: measured in pixels per frame
-        """
-        start_x = start_point[0]
-        start_y = start_point[1]
-        end_x = end_point[0]
-        end_y = end_point[1]
-        return round(math.sqrt(abs(end_x - start_x) ** 2 + abs(end_y - start_y) ** 2), 4)
-
-    def calc_direction(self, start_point, end_point) -> float:
-        """
-        This functions takes in a start_point and end_point and calculates the counterclockwise angle
-        by treating the start_point as the origin and then finding what angle is formed by the positive x-axis
-        of that "origin"(a.k.a the start_point) and the straight line between the start_point and end_point.
-        :param start_point: Tuple of form: (x, y)
-        :param end_point: Tuple of form: (x, y)
-        :return: direction: measured in radians
-        """
-        return round(math.atan2(-(end_point[1] - start_point[1]), end_point[0] - start_point[0]), 4)
 
     def __str__(self):
         return '{' + '\n' \
                 + '\t' + 'frames: ' + str(self.start_frame.frame_id) + '-' + str(self.end_frame.frame_id) + ',' + '\n' \
-                + '\t' + 'translate_detected: ' + str(self.translate_detected) + '\n' \
-                + '\t' + 'delta_x: ' + str(self.delta_x) + ',' + '\n' \
-                + '\t' + 'delta_y: ' + str(self.delta_y) + ',' + '\n' \
                 + '\t' + 'motion_vector: ' + str(self.motion_vector) + '\n' \
                 + '\t' + 'velocity: ' + str(self.velocity) + ' pixels/frame' + '\n' \
-                + '\t' + 'direction: ' + str(self.direction) + ' radians' + '\n' \
-                + '\t' + 'transform_detected: ' + str(self.transform_detected) + '\n' \
                 + '\t' + 'diag_angle_change: ' + str(self.diag_angle_change) + ' radians' + '\n' \
-                + '\t' + 'diag_length_change: ' + str(self.diag_length_change) + ' pixels' + '\n' \
-                + '\t' + 'height_change: ' + str(self.height_change) + ' pixels' + '\n' \
-                + '\t' + 'width_change: ' + str(self.width_change) + ' pixels' + '\n' \
                 + '}'
